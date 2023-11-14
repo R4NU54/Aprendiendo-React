cc35e0462d3e3c23aefca147561e541b3ccb8ab1
Raúl Núñez <r4nu54@gmail.com>
2023-11-14 14:39:14 +0100

    End secction 4 vite

---
+++ 04-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+
+export const FirstApp = ({ title, subTitle, name }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{subTitle}</p>
+      <p>{name}</p>
+    </div>
+  )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+  name: PropTypes.string
+}
+
+FirstApp.defaultProps = {
+  title: 'Titulo vacio',
+  subTitle: 'Subtitulo vacio',
+  name: 'Raúl Núñez'
+}
