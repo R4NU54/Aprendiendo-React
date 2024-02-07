e52ed6d8e318a28458c4d5865ad70c97a939cd2f
Raúl Núñez <raul.nunez.santiago@outlook.es>
2024-02-07 18:12:04 +0100

    Add initial files for Hooks App

---
+++ 06-hooks-app/src/HooksApp.jsx
@@ -0,0 +1,16 @@
+// import { CounterApp } from './01-useState/CounterApp';
+
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+
+export const HooksApp = () => {
+  return (
+    <>
+      <h1>Hooks App</h1>
+
+      <hr />
+
+      {/* <CounterApp /> */}
+      <CounterWithCustomHook />
+    </>
+  );
+};
