9b1a414a5564c44377edd4cf6be702e6338f5540
Raúl Núñez <r4nu54@gmail.com>
2024-02-07 16:52:47 +0100

    Add Jest configuration and dependencies

--- 05-gif-app/.eslintrc.js
+++ 05-gif-app/.eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
@@ -10,11 +10,8 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jest'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
